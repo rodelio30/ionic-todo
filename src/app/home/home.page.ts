6fa5525dc92dde413e5fd3e2a24521837f11d18e
rodelio30 <rodeliobdomingojr@gmail.com>
2022-02-11 12:02:14 +0800

    viewing specific verse

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  secJohn: any;
+  public title = '2 John';
   constructor() {}
 
+  ngOnInit(){
+    fetch('./assets/inputFile/newbible.json').then(res => res.json()).then(json => {
+      this.secJohn = json.filter(item => item.book_name === '2 John');
+    });
+    // this.results = this.results.filter(item => item.book_name === '2 John');
+  }
 }
