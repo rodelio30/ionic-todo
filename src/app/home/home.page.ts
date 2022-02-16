7b0ece4de672193f78174db27443f851016522ff
rodelio30 <rodeliobdomingojr@gmail.com>
2022-02-16 14:47:55 +0800

    swiper page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperOptions } from 'swiper/types/swiper-options';
+
 
 @Component({
   selector: 'app-home',
@@ -6,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  public swiperConfig: SwiperOptions = {};
   secJohn: any;
   public title = '2 John';
   constructor() {}
 
   ngOnInit(){
     fetch('./assets/inputFile/newbible.json').then(res => res.json()).then(json => {
-      this.secJohn = json.filter(item => item.book_name === '2 John');
+      this.secJohn = json.filter(item => item.bookName === '2 John');
     });
     // this.results = this.results.filter(item => item.book_name === '2 John');
   }
